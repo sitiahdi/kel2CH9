7c353bf26c673b6bbe127d3c8b28d952b5e9b796
Muhammad Afif Fauzi <afiffauzi234@gmail.com>
2022-07-27 19:17:42 +0700

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import ErrHandlePage from "./views/ErrHandle";
 import GameListPage from "./views/GameListPage";
 import GameDetailsPage from "./views/GameDetailsPage";
-import Register from './views/Register';
-import Login from './views/login';
+import Register from "./views/Register";
+import Login from "./views/login";
 import Profile from "./views/Profile";
+import Landing from "./views/Landing";
 
 // Tambah route baru (tambahkan diatas ErrHandlePage)
 // <Route path="/" element={<Component Page nya />}/>
@@ -17,9 +18,9 @@ function App() {
     <div>
       <Router>
         <Routes>
-
-          <Route path="/game-list" element={<GameListPage />}/>
-          <Route path="/game-details/:userId" element={<GameDetailsPage />}/>
+          <Route path="/" exact element={<Landing />} />
+          <Route path="/game-list" element={<GameListPage />} />
+          <Route path="/game-details/:userId" element={<GameDetailsPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
